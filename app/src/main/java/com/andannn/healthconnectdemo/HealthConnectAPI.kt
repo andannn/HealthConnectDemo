34a9962b47e45a00610eb6d9124217a538aecb3a
andannn <jqn296763005@gmail.com>
2025-02-06 10:37:01 +0800

    handle exception

--- app/src/main/java/com/andannn/healthconnectdemo/HealthConnectAPI.kt
+++ app/src/main/java/com/andannn/healthconnectdemo/HealthConnectAPI.kt
@@ -1,24 +1,33 @@
 package com.andannn.healthconnectdemo
 
 import android.content.Context
+import android.os.RemoteException
 import android.util.Log
 import androidx.health.connect.client.HealthConnectClient
 import androidx.health.connect.client.records.StepsRecord
 import androidx.health.connect.client.request.ReadRecordsRequest
 import androidx.health.connect.client.time.TimeRangeFilter
+import java.io.IOException
 import java.time.Instant
 
 private const val TAG = "HealthConnectAPI"
+
+class ClientUnavailableException(override val message: String? = null) : Throwable()
+class RemoteApiFailedException(override val message: String? = null) : Throwable()
 
 interface HealthConnectAPI {
     suspend fun foo()
 
     suspend fun getGrantedPermissions(): Set<String>
 
+    /**
+     * @throws RemoteApiFailedException
+     * @throws ClientUnavailableException
+     */
     suspend fun readStepsByTimeRange(
         startTime: Instant,
         endTime: Instant
-    )
+    ) : List<StepsRecord>
 }
 
 class HealthConnectAPIImpl(
@@ -38,17 +47,27 @@ class HealthConnectAPIImpl(
         startTime: Instant,
         endTime: Instant
     ) = withClientOrThrow(context) { client ->
-        Log.d(TAG, "readStepsByTimeRange: $startTime, $endTime")
-        val response =
+        val response = runCatchingRemoteApiErrors {
             client.readRecords(
                 ReadRecordsRequest(
                     StepsRecord::class,
                     timeRangeFilter = TimeRangeFilter.between(startTime, endTime)
                 )
             )
-        for (stepRecord in response.records) {
-            // Process each step record
-            Log.d(TAG, "readStepsByTimeRange:response  ${stepRecord}")
+        }
+
+        response.records
+    }
+
+    private inline fun <T> runCatchingRemoteApiErrors(block: () -> T): T {
+        try {
+            return block()
+        } catch (e: RemoteException) {
+            throw RemoteApiFailedException(e.message)
+        } catch (e: IOException) {
+            throw RemoteApiFailedException(e.message)
+        } catch (e: SecurityException) {
+            throw RemoteApiFailedException(e.message)
         }
     }
 }
@@ -61,22 +80,10 @@ private suspend fun <T> withClientOrThrow(
     val availabilityStatus = HealthConnectClient.getSdkStatus(context, providerPackageName)
     if (availabilityStatus == HealthConnectClient.SDK_UNAVAILABLE) {
         Log.d(TAG, "checkIfHealthConnectInstalled: SDK_UNAVAILABLE")
-        throw IllegalStateException("SDK_UNAVAILABLE")
+        throw ClientUnavailableException("SDK_UNAVAILABLE")
     }
     if (availabilityStatus == HealthConnectClient.SDK_UNAVAILABLE_PROVIDER_UPDATE_REQUIRED) {
-//        Log.d(TAG, "checkIfHealthConnectInstalled: SDK_UNAVAILABLE_PROVIDER_UPDATE_REQUIRED")
-//        // Optionally redirect to package installer to find a provider, for example:
-//        val uriString =
-//            "market://details?id=$providerPackageName&url=healthconnect%3A%2F%2Fonboarding"
-//        context.startActivity(
-//            Intent(Intent.ACTION_VIEW).apply {
-//                setPackage("com.android.vending")
-//                data = Uri.parse(uriString)
-//                putExtra("overlay", true)
-//                putExtra("callerId", context.packageName)
-//            }
-//        )
-        throw IllegalStateException("SDK_UNAVAILABLE_PROVIDER_UPDATE_REQUIRED")
+        throw ClientUnavailableException("SDK_UNAVAILABLE_PROVIDER_UPDATE_REQUIRED")
     }
     Log.d(TAG, "checkIfHealthConnectInstalled: available")
     val healthConnectClient = HealthConnectClient.getOrCreate(context)
